260eb1a20906b417a6b0a42c6e592e3cb4136060
Omar Sahyoun <osahyoun@gmail.com>
2012-05-17 20:30:05 +0200

    Ditch rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SunrizeBadge::Application.config.secret_token = '85f92d827d1ca9babc6d2be1bc45e0f109d2a1a5c3f96f7f50687669ce6246f6257cbebd7542dd207a064d1b08b093a978b48c324612863ba64308df12e7e942'
